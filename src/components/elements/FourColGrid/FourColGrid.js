f07e1342fa45d2378acee7fa531cc1a84b6a335d
Koray Dundar <koraydundar_40@hotmail.com>
2019-05-05 18:17:24 +0300

    Checked PropTypes

--- src/components/elements/FourColGrid/FourColGrid.js
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './FourColGrid.css'
 
 const FourColGrid = (props) => {
@@ -23,6 +24,11 @@ const FourColGrid = (props) => {
             </div>
         </div>
     )
+}
+
+FourColGrid.PropTypes = {
+    header: PropTypes.string,
+    loading: PropTypes.bool
 }
 
 export default FourColGrid
